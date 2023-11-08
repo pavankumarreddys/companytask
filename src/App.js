8a16d09c074ecd12ce2bb23a37def7b1ecc8a2f1
Pavan Kumar Reddy Sannala <pavanreddyr34@gmail.com>
2023-11-08 22:40:17 +0530

    new

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Home from './components/Home/Home';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <Home/>
+  );
+}
+
+export default App;
